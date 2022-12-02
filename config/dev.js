50692e0c771de6572e207c088b31cf3d6b60a839
Krishan <79534931+Krishan-Luhach@users.noreply.github.com>
2022-12-02 10:10:44 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    MONGOURL:"mongodb+srv://krishan:zvYVLxNa1x0Ezsjq@cluster0.yxvso.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET:"effegtaoglalsn",
-    SENDGRID_API:"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",
-    EMAIL:"http://localhost:3000"
-}
